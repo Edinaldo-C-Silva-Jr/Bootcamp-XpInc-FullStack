f9ecbff0a21bd4147d2363c54153d0c712295d41
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-01 15:55:10 -0300

    Adiciona uma rota para testar o login, utilizando uma classe LoginDTO e uma rota no PRogram.cs.

--- Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Program.cs
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Program.cs
@@ -1,6 +1,23 @@
+using MinimalsAPI.Dominio.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Rota padrão do aplicativo.
 app.MapGet("/", () => "Hello World!");
+
+// Rota para realizar o login.
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login realizado com sucesso.");
+    }
+    else
+    {
+        return Results.Unauthorized();
+        ;
+    }
+});
 
 app.Run();
