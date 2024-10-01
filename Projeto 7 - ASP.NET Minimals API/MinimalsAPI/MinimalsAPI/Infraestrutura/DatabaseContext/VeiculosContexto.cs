2bd4c4c3838f477f3d6b38328057c0be60d42ac2
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-01 16:54:04 -0300

    Adiciona o DBContext para conexão com o banco de dados.

---
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Infraestrutura/DatabaseContext/VeiculosContexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalsAPI.Dominio.Entidades;
+
+namespace MinimalsAPI.Infraestrutura.DatabaseContext
+{
+    public class VeiculosContexto : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public VeiculosContexto(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Administrador> Administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("MinimalsAPIDatabase"));
+        }
+    }
+}
