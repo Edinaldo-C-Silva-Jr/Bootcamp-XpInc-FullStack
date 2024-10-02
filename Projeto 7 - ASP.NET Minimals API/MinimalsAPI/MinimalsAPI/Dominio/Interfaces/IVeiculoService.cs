65c2ed51ace372fe73bdce261557903e359b9e67
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-02 16:31:28 -0300

    Adiciona a interface e classe do VeiculoService para fazer consultas com véiculos.

---
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Dominio/Interfaces/IVeiculoService.cs
@@ -0,0 +1,17 @@
+﻿using MinimalsAPI.Dominio.Entidades;
+
+namespace MinimalsAPI.Dominio.Interfaces
+{
+    public interface IVeiculoService
+    {
+        List<Veiculo> RetornarTodos(int pagina = 1, string? nome = null, string? marca = null);
+
+        Veiculo? RetornaPorId(int id);
+
+        void IncluirVeiculo(Veiculo veiculo);
+
+        void AtualizarVeiculo(Veiculo veiculo);
+
+        void DeletarVeiculo(Veiculo veiculo);
+    }
+}
---
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Dominio/Servicos/VeiculoService.cs
@@ -0,0 +1,58 @@
+﻿using MinimalsAPI.Dominio.Entidades;
+using MinimalsAPI.Dominio.Interfaces;
+using MinimalsAPI.Infraestrutura.DatabaseContext;
+
+namespace MinimalsAPI.Dominio.Servicos
+{
+    public class VeiculoService : IVeiculoService
+    {
+        /// <summary>
+        /// Recebe o contexto do banco de dados por DI.
+        /// </summary>
+        private readonly VeiculosContexto _context;
+
+        public VeiculoService(VeiculosContexto context)
+        {
+            _context = context;
+        }
+
+        public List<Veiculo> RetornarTodos(int pagina = 1, string? nome = null, string? marca = null)
+        {
+            var query = _context.Veiculos.AsQueryable();
+
+            if (!string.IsNullOrEmpty(nome))
+            {
+                query = query.Where(v => v.Nome.Contains(nome, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            int itensPorPagina = 10;
+
+            query = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+
+            return query.ToList();
+        }
+
+        public Veiculo? RetornaPorId(int id)
+        {
+            return _context.Veiculos.Where(v => v.Id == id).FirstOrDefault();
+        }
+
+        public void IncluirVeiculo(Veiculo veiculo)
+        {
+            _context.Veiculos.Add(veiculo);
+            _context.SaveChanges();
+        }
+
+        public void AtualizarVeiculo(Veiculo veiculo)
+        {
+            _context.Veiculos.Update(veiculo);
+            _context.SaveChanges();
+        }
+
+        public void DeletarVeiculo(Veiculo veiculo)
+        {
+            _context.Veiculos.Remove(veiculo);
+            _context.SaveChanges();
+        }
+    }
+}
