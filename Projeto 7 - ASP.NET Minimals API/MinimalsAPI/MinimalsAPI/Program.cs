d5ae62792a82f8365102e9ac7d5e585f09b49e77
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-13 14:48:06 -0300

    Mapeia as rotas de GET por Id, PUT e DELETE para os veiculos.

--- Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Program.cs
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPI/Program.cs
@@ -31,8 +31,8 @@ internal class Program
         var app = builder.Build();
         #endregion
 
-        // Mapeia a rota inicial do aplicativo através da model Home, que aponta para a documentação.
-        app.MapGet("/", () => Results.Json(new Home()));
+        // Mapeia a rota inicial através da model Home, que aponta para a documentação.
+        app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 
         // Rota para realizar o login.
         app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorService administradorService) =>
@@ -45,9 +45,9 @@ internal class Program
             {
                 return Results.Unauthorized();
             }
-        });
+        }).WithTags("Administradores");
 
-        // Rota para realizar o cadastro de um veículo.
+        // Rota para realizar o cadastro de um veículo através de um DTO.
         app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoService veiculoService) =>
         {
             Veiculo veiculo = new()
@@ -60,7 +60,7 @@ internal class Program
             veiculoService.IncluirVeiculo(veiculo);
 
             return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
-        });
+        }).WithTags("Veiculos");
 
         // Rota para retornar todos os veículos.
         app.MapGet("/veiculos", (IVeiculoService veiculoService, int pagina = 1) =>
@@ -68,7 +68,60 @@ internal class Program
             List<Veiculo> veiculos = veiculoService.RetornarTodos(pagina);
 
             return Results.Ok(veiculos);
-        });
+        }).WithTags("Veiculos");
+
+        // Rota para retornar um veículo através do ID.
+        app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoService veiculoService) =>
+        {
+            Veiculo? veiculo = veiculoService.RetornaPorId(id);
+
+            if (veiculo is null)
+            {
+                return Results.NotFound();
+            }
+            else
+            {
+                return Results.Ok(veiculo);
+            }
+        }).WithTags("Veiculos");
+
+        // Rota para alterar um veículo através do ID e de um veiculoDTO.
+        app.MapPut("/veiculos/{id}", ([FromRoute] int id, [FromRoute] VeiculoDTO veiculoDTO, IVeiculoService veiculoService) =>
+        {
+            Veiculo? veiculo = veiculoService.RetornaPorId(id);
+
+            if (veiculo is null)
+            {
+                return Results.NotFound();
+            }
+            else
+            {
+                veiculo.Nome = veiculoDTO.Nome;
+                veiculo.Marca = veiculoDTO.Marca;
+                veiculo.Ano = veiculoDTO.Ano;
+
+                veiculoService.AtualizarVeiculo(veiculo);
+
+                return Results.Ok(veiculo);
+            }
+        }).WithTags("Veiculos");
+
+        // Rota para deletar um veículo através do ID.
+        app.MapDelete("/veiculos/{id}", ([FromRoute] int id, IVeiculoService veiculoService) =>
+        {
+            Veiculo? veiculo = veiculoService.RetornaPorId(id);
+
+            if (veiculo is null)
+            {
+                return Results.NotFound();
+            }
+            else
+            {
+                veiculoService.DeletarVeiculo(veiculo);
+
+                return Results.NoContent();
+            }
+        }).WithTags("Veiculos");
 
         // Configura o aplicativo para usar o Swagger com a interface.
         app.UseSwagger();
