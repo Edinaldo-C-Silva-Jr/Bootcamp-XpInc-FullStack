87037d9a40c9d43a1f15ff4f9387f9bf7d067350
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-16 13:48:13 -0300

    Cria um projeto de teste MinimalsAPITeste. Adiciona testes para Administrador e Veiculo.

---
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPITeste/Dominio/Entidades/AdministradorTeste.cs
@@ -0,0 +1,27 @@
+﻿using MinimalsAPI.Dominio.Entidades;
+
+namespace MinimalsAPITeste.Dominio.Entidades
+{
+    [TestClass]
+    public class AdministradorTeste
+    {
+        [TestMethod]
+        public void TestarGetSetPropriedades()
+        {
+            // Arrange
+            Administrador administrador = new();
+
+            // Act
+            administrador.Id = 1;
+            administrador.Email = "admin@teste.com";
+            administrador.Senha = "12345678";
+            administrador.Perfil = "Admin";
+
+            // Assert
+            Assert.AreEqual(1, administrador.Id);
+            Assert.AreEqual("admin@teste.com", administrador.Email);
+            Assert.AreEqual("12345678", administrador.Senha);
+            Assert.AreEqual("Admin", administrador.Perfil);
+        }
+    }
+}
---
+++ Projeto 7 - ASP.NET Minimals API/MinimalsAPI/MinimalsAPITeste/Dominio/Entidades/VeiculoTeste.cs
@@ -0,0 +1,27 @@
+﻿using MinimalsAPI.Dominio.Entidades;
+
+namespace MinimalsAPITeste.Dominio.Entidades
+{
+    [TestClass]
+    public class VeiculoTeste
+    {
+        [TestMethod]
+        public void TestarGetSetPropriedades()
+        {
+            // Arrange
+            Veiculo veiculo = new();
+
+            // Act
+            veiculo.Id = 1;
+            veiculo.Nome = "Carro";
+            veiculo.Marca= "Marca de Carro";
+            veiculo.Ano = 2010;
+
+            // Assert
+            Assert.AreEqual(1, veiculo.Id);
+            Assert.AreEqual("Carro", veiculo.Nome);
+            Assert.AreEqual("Marca de Carro", veiculo.Marca);
+            Assert.AreEqual(2010, veiculo.Ano);
+        }
+    }
+}
