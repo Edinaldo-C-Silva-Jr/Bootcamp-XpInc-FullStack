7b55c03b1310ed66da06f5b8f269629d10e19313
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-30 22:09:40 -0300

    Adiciona um projeto de teste em React para a Aula 35.

---
+++ Aula 35 - Primeiros Passos na Biblioteca React/primeiro-projeto-react/src/App.js
@@ -0,0 +1,15 @@
+import Button from './components/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React!</h1>
+      <Button title="Entrar" />
+      <Button title="Sair" />
+      <Button title="Abrir" />
+      <Button title="Salvar" />
+    </div>
+  );
+}
+
+export default App;
