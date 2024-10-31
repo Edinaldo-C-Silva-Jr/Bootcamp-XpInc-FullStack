7c2ac4e26675331b30173fc9abe4511baea4e903
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-31 17:42:50 -0300

    Implementa os botões e ajusta a estilização dos componentes.

--- Projeto 08 - Criando uma Calculadora em React/calculadora/src/components/Button/styles.js
+++ Projeto 08 - Criando uma Calculadora em React/calculadora/src/components/Button/styles.js
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    margin: 5px;
+    width: 20%;
+    min-width: 100px;
+    height: 20%;
+    min-height: 40px;
+    border: 2px solid #B0B0CF;
+    border-radius: 8px;
+    background-color: #E0E0FF;
+    font-size: 30px;
+`
--- Projeto 08 - Criando uma Calculadora em React/calculadora/src/components/Input/styles.js
+++ Projeto 08 - Criando uma Calculadora em React/calculadora/src/components/Input/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const InputContainer = styled.div`
     width = 100%;
-    height: 60px;
+    height: 50px;
     background-color: #90F0A0;
 
     display: flex;
@@ -11,7 +11,8 @@ export const InputContainer = styled.div`
 
     input {
         width: 96%;
-        height: 40px;
+        height: 30px;
+        border-radius: 6px;
         background-color: #80E090;
         font-size: 30px;
         border: 0;
