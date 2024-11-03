84c809db452190e3e1341874de583071fdac1e65
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-11-03 15:45:36 -0300

    Cria o componente de Input para pesquisar por repositórios.

---
+++ Projeto 11 - Criando uma Wiki de Repos do Github/wiki-github/src/components/Input/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    border: 1px solid #fafafa;
+    border-radius: 20px;
+    margin: 20px;
+    height: 62px;
+    width: 90%;
+    
+    input {
+        border: 0;
+        width: 100%;
+        height: 62px;
+        border-radius: 20px;
+
+        background: transparent;
+        color: #ffffff;
+        font-size: 32px;
+    }
+`
--- Projeto 11 - Criando uma Wiki de Repos do Github/wiki-github/src/pages/Home/index.js
+++ Projeto 11 - Criando uma Wiki de Repos do Github/wiki-github/src/pages/Home/index.js
@@ -1,10 +1,14 @@
-import gitLogo from '../../assets/github-logo.png';
-import { Container } from './styles'
+import gitLogo from "../../assets/github-logo.png";
+import { Container } from "./styles";
+import Input from '../../components/Input'
 
 const App = () => {
-    return <Container className="App">
-        <img src={gitLogo} width={72} height={72} alt="logo"></img>
-    </Container>;
+    return (
+        <Container className="App">
+            <img src={gitLogo} width={72} height={72} alt="logo"></img>
+            <Input />
+        </Container>
+    );
 };
 
 export default App;
