7e5a99cc805d4108049a80ef904620831889ca25
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-11-04 15:28:58 -0300

    Adiciona um estilo global à aplicação.

--- Aula 41 - Estilização com Styled Components/clone-plataforma-dio/src/pages/Home/index.js
+++ Aula 41 - Estilização com Styled Components/clone-plataforma-dio/src/pages/Home/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 
 const Home = () => {
     return (
         <div>
             <Header />
-            <Button title="Teste 1" />
-            <Button variant="secondary" title="Teste 2" />
         </div>
     );
 }
