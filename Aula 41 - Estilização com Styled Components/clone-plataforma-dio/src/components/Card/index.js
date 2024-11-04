2534379b85bfba710f757837fc3ab534eda7f74c
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-11-04 18:34:51 -0300

    Cria o componente de Card.

---
+++ Aula 41 - Estilização com Styled Components/clone-plataforma-dio/src/components/Card/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FiThumbsUp } from "react-icons/fi";
+
+import {
+    CardContainer,
+    Content,
+    HasInfo,
+    ImageBackground,
+    PostInfo,
+    UserInfo,
+    UserPicture,
+} from "./styles";
+
+const Card = () => {
+    return (
+        <CardContainer>
+            <ImageBackground />
+            <Content>
+                <UserInfo>
+                    <UserPicture />
+                    <section>
+                        <h4>Edinaldo Silva</h4>
+                        <p>Há 8 minutos</p>
+                    </section>
+                </UserInfo>
+                <PostInfo>
+                    <h4>Projeto para curso de HTML e CSS.</h4>
+                    <p>
+                        Projeto feuto no curso de HTML e CSS no bootcamp XP Inc
+                        - Desenvolvimento Full Stack...
+                        <strong>Saiba Mais</strong>
+                    </p>
+                </PostInfo>
+                <HasInfo>
+                    <h4>#HTML #CSS #JavaScript</h4>
+                    <p>
+                        <FiThumbsUp /> 10
+                    </p>
+                </HasInfo>
+            </Content>
+        </CardContainer>
+    );
+};
+
+export { Card };
