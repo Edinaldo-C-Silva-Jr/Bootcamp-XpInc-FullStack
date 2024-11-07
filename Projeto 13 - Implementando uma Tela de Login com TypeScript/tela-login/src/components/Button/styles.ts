17325596f93a2b62048b1b1b4ee8687c770402a1
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-11-07 13:29:13 -0300

    Cria o componente de Button e a estrutura da página de Login.

---
+++ Projeto 13 - Implementando uma Tela de Login com TypeScript/tela-login/src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    width: 80%;
+    height: 40px;
+    background-color: #81259d;
+    color: #ffffff;
+
+    border: 1px solid #a532b8;
+    border-radius: 20px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor: pointer;
+    }
+`;
---
+++ Projeto 13 - Implementando uma Tela de Login com TypeScript/tela-login/src/pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import Button from "../../components/Button";
+
+const Login = () => {
+    return <div>
+        <Button title="Botão"></Button>
+    </div>;
+};
+
+export default Login;
