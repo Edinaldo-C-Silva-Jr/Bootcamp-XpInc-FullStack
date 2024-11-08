69cd72eb81d994b02c850414e567eb6dcefe10ff
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-11-08 20:04:15 -0300

    Conserta alguns erros iniciais no projeto.

--- Projeto 14 - Criando um Blog com NextJS/Blog DIO/services/api.js
+++ Projeto 14 - Criando um Blog com NextJS/Blog DIO/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://nqowemdzgcywxcxegltq.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xb3dlbWR6Z2N5d3hjeGVnbHRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwOTM4OTcsImV4cCI6MjA0NjY2OTg5N30.SK6PZToGMQP11nUYEgBSTavnJ6-Blk4LVNzWraSA68Y",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xb3dlbWR6Z2N5d3hjeGVnbHRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwOTM4OTcsImV4cCI6MjA0NjY2OTg5N30.SK6PZToGMQP11nUYEgBSTavnJ6-Blk4LVNzWraSA68Y"
     }
 })
